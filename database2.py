9dabd68a1dc744e6e1b85797f43c27f5c898635e
Lokesh <63948472+LokeshNaidu8@users.noreply.github.com>
2022-04-06 23:31:04 +0530

    Changes made....Check it out

--- database2.py
+++ database2.py
@@ -1,15 +1,21 @@
 import pymysql as p
+
+
 def getconnection():
-    return p.connect(host="localhost",user="root",password="",database="university")
+    return p.connect(host="localhost", user="root", password="", database="university")
+
+
 def adddata(t):
-    con=getconnection()
-    cur=con.cursor()
-    query1="insert into contact(name,email,subject,message)values(%s,%s,%s,%s)"
-    cur.execute(query1,t)
+    con = getconnection()
+    cur = con.cursor()
+    query1 = "insert into contact(name,email,subject,message)values(%s,%s,%s,%s)"
+    cur.execute(query1, t)
     con.commit()
     con.close()
 
 # for validate user login
+
+
 def fetchuserpwd():
     con = getconnection()
     cur = con.cursor()
@@ -20,32 +26,42 @@ def fetchuserpwd():
     con.close()
     return f
 
+
+def deleteUser(id):
+    con = getconnection()
+    cur = con.cursor()
+    query = f"delete from contact where id={id}"
+    cur.execute(query)
+    f = cur.fetchall()
+    con.commit()
+    con.close()
+    return f
+
+
 def fetchdata():
-    con=getconnection()
-    cur=con.cursor()
+    con = getconnection()
+    cur = con.cursor()
     cur.execute("select * from contact")
-    datalist=cur.fetchall()
+    datalist = cur.fetchall()
     con.commit()
     con.close()
     return datalist
 
+
 def specificdata(id):
-    con=getconnection()
-    cur=con.cursor()
-    cur.execute("select * from contact where id=%s",(id,))
-    datalist=cur.fetchall()
+    con = getconnection()
+    cur = con.cursor()
+    cur.execute("select * from contact where id=%s", (id,))
+    datalist = cur.fetchall()
     con.commit()
     con.close()
     return datalist[0]
 
 
 def deletedata(id):
-    con=getconnection()                  
-    cur=con.cursor()
-    query3="delete from contact where id=%s"
-    cur.execute(query3,(id,))
+    con = getconnection()
+    cur = con.cursor()
+    query3 = "delete from contact where id=%s"
+    cur.execute(query3, (id,))
     con.commit()
     con.close()
-    
-
-
--- flask1.py
+++ flask1.py
@@ -1,58 +1,80 @@
-from flask import Flask, redirect, render_template,request,session
-from database2 import adddata,fetchuserpwd,fetchdata
+from flask import Flask, redirect, render_template, request, session
+from database2 import adddata, fetchuserpwd, fetchdata, deleteUser
 
-#create object of the flask class
-app=Flask(__name__)
+# create object of the flask class
+app = Flask(__name__)
+
+
 @app.route("/")
 def index():
+    session.clear()
     return render_template("index.html")
+
 
 @app.route("/home")
 def home():
     return render_template("index.html")
 
+
 @app.route("/admins")
 def admin():
     return render_template("admin.html")
+
 
 @app.route("/abouts")
 def about():
     return render_template("about.html")
 
+
 @app.route("/blogs")
 def blog():
     return render_template("blog.html")
+
 
 @app.route("/contacts")
 def contact():
     return render_template("contact.html")
 
+
 @app.route("/courses")
 def course():
     return render_template("course.html")
 
+
 @app.route("/studentablelink")
 def homefun():
-    f = fetchdata()
-    return render_template("studentable.html",data = f)
+    if(session.get('name')):
+        f = fetchdata()
+        return render_template("studentable.html", data=f)
+    else:
+        return redirect("/validlogin")
 
 
-@app.route("/savelink",methods=["POST"])
+@app.route("/savelink", methods=["POST"])
 def save():
-    name=request.form["name"]
-    email=request.form["email"]
-    subject=request.form["subject"]
-    message=request.form["message"]
-    t=(name,email,subject,message)
+    name = request.form["name"]
+    email = request.form["email"]
+    subject = request.form["subject"]
+    message = request.form["message"]
+    t = (name, email, subject, message)
     adddata(t)
     return redirect("contacts")
 
 
+@app.route("/deleteuser/<int:id>")
+def DELETE_USER(id):
+    if(request.method == "GET"):
+        deleteUser(id)
+    f = fetchdata()
+    return render_template("studentable.html", data=f)
+
+
 # validate user login.html
-@app.route("/validlogin", methods = ["GET","POST"])
+@app.route("/validlogin", methods=["GET", "POST"])
 def validloginfun():
+    session.clear()
     error = None
-    if (request.method =="POST"):
+    if (request.method == "POST"):
         session['name'] = request.form.get("name")
         session['pwd'] = request.form.get("password")
         f = fetchuserpwd()
@@ -65,6 +87,6 @@ def validloginfun():
     return render_template("admin.html")
 
 
-   
-if __name__=="__main__":
+if __name__ == "__main__":
+    app.secret_key = "super secret key"
     app.run(debug=True)
